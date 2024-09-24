b22b5dc1851b43c5d4b617e09c23eb939eeeff40
itskarelleh <karellehofler@gmail.com>
2024-09-23 23:19:18 -0400

    FRONTEND | Implement Caching Strategy Fixes #1

--- react-native/public/sw.js
+++ react-native/public/sw.js
@@ -1,21 +1,59 @@
 const CACHE_NAME = 'taino-language-app-cache-v1';
-const urlsToCache = [
+const STATIC_ASSETS = [
   '/',
   '/index.html',
-  '/static/js/bundle.js',
-  '/manifest.json'
+  '/static/js/main.js',
+  '/static/css/main.css',
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+      .then(cache => cache.addAll(STATIC_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        // If this is a navigation request, use network-first strategy
+        if (event.request.mode === 'navigate') {
+          const copy = response.clone();
+          event.waitUntil(
+            caches.open(CACHE_NAME)
+              .then(cache => cache.put(event.request, copy))
+          );
+          return response;
+        }
+        
+        // For other requests, use cache-first strategy
+        return caches.open(CACHE_NAME).then(cache => {
+          return cache.match(event.request).then(cachedResponse => {
+            if (cachedResponse) {
+              return cachedResponse;
+            }
+            return response;
+          });
+        });
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
   );
 });
