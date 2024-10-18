3c533fab24bb87f528b4902752d4e8dad00eecac
itskarelleh <karellehofler@gmail.com>
2024-10-17 21:03:43 -0400

    Change to app router Fixes #11

--- react-frontend/app/index.tsx
+++ react-frontend/app/index.tsx
@@ -2,10 +2,13 @@ import axios from "axios";
 import * as React from "react";
 import { View, Text, Button } from "react-native";
 import { StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
-export const Home = ({ navigation }) => {
+export const Page = () => {
 
   const [ meow, setMeow ] = React.useState("");
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
 
   const fetchMeow = async () => {
     console.log("Meow!");
@@ -22,12 +25,9 @@ export const Home = ({ navigation }) => {
       <Text>Home Screen</Text>
       <Button
         title="Go to Jane's profile"
-        onPress={() =>
-          navigation.navigate('Profile', {name: 'Jane'})
-        }
+        onPress={() => navigation.navigate('profile', { name: 'Jane' })}
       />
       <Button title="Meow? 🐱" onPress={fetchMeow} />
-
       <Text>{meow}</Text>
     </View>
   );
@@ -39,8 +39,5 @@ const styles = StyleSheet.create({
       backgroundColor: '#25292e',
       alignItems: 'center',
       justifyContent: 'center',
-    },
-  });
-
-  
-  
+    }
+});
--- react-frontend/index.js
+++
@@ -1,9 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-
-AppRegistry.registerComponent('App', () => App);
-
-if (typeof document !== 'undefined') {
-  const rootTag = document.getElementById('root');
-  AppRegistry.runApplication('App', { rootTag });
-}
