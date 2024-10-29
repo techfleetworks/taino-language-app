bd161c18b6bac22ad5a28cb5e49b6c6e3714b673
Linh Nguyen <nguyenthuylinh280105@gmail.com>
2024-10-28 23:49:48 -0400

    add font weight

--- react-frontend/constants/Text.tsx
+++ react-frontend/constants/Text.tsx
@@ -2,18 +2,22 @@ export default {
     heading1: {
         fontSize: '32px',
         margin: 'auto',
+        fontWeight: '700',
     },
     heading2: {
         fontSize: '24px',
         margin: 'auto',
+        fontWeight: '700',
     },
     button1: {
         fontSize: '20px',
         margin: 'auto',
+        fontWeight: '600',
     },
     button2: {
         fontSize: '16px',
         margin: 'auto',
+        fontWeight: '600',
     },
     body: {
         fontSize: '16px',
@@ -26,5 +30,6 @@ export default {
     button2Underlined: {
         fontSize: '16px',
         margin: 'auto',
+        fontWeight: '600',
     }
 }
