e72e8b73bcd2b10d864bfe7f32dfe4afb1e61f13
Linh Nguyen <nguyenthuylinh280105@gmail.com>
2024-10-28 22:22:30 -0400

    add constant text styles

---
+++ react-frontend/constants/Text.tsx
@@ -0,0 +1,28 @@
+export default {
+    h1: {
+        fontSize: '32px',
+        margin: 'auto',
+    },
+    h2: {
+        fontSize: '24px',
+        margin: 'auto',
+    },
+    button1: {
+        fontSize: '20px',
+        margin: 'auto',
+    },
+    button2: {
+        fontSize: '16px',
+        margin: 'auto',
+    },
+    body: {
+        fontSize: '16px',
+        lineHeight: '1.75',
+    },
+    caption: {
+        fontSize: '14px',
+        margin: 'auto',
+    },
+
+    
+}
