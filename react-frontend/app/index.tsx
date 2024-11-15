de3d3c896195818701fde0de37ff29d2e02e10d2
neisha rose <neisha1618@gmail.com>
2024-11-14 20:04:33 -0600

    the issue was already fixed

--- react-frontend/app/index.tsx
+++ react-frontend/app/index.tsx
@@ -56,7 +56,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems  : "center",
     gap: 64,
     backgroundColor: Colors.background,
 
